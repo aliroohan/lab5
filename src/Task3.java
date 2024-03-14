6f774cf0210b59d6fb2b8f87513407e96644f7e8
aliroohan <aliroohan321@gmail.com>
2024-03-14 13:06:11 +0500

    Added Task 3

--- src/Task3.java
+++ src/Task3.java
@@ -1,2 +1,40 @@
+import java.util.ArrayList;
+
 public class Task3 {
+    public static void main(String[] args) {
+        ArrayList<String> student = new ArrayList<>();
+        student.add("Ali");
+        student.add("Ahmed");
+        student.add("Umer");
+        student.add("Talha");
+
+        displayStudents(student);
+        System.out.println();
+
+        student.add("Tooba");
+        student.add("Waleed");
+        displayStudents(student);
+
+        System.out.println();
+        System.out.println("Size : " + student.size());
+        System.out.println();
+
+        student.add(0, "Hamza");
+        student.add(1, "Rizwan");
+
+        student.sort(null);
+
+        displayStudents(student);
+        System.out.println();
+        student.remove("Tooba");
+        displayStudents(student);
+        student.remove(student.size()-1);
+        System.out.println();
+        displayStudents(student);
+    }
+    public static void displayStudents(ArrayList<String> student){
+        for (String s : student) {
+            System.out.println(s);
+        }
+    }
 }
